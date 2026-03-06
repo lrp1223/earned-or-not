3c3dab120d4f245b56cb68cdcd5fc7b85812cd74
18million <584304337@qq.com>
2026-03-06 17:27:38 +0800

    修复：编辑时带出原有数据，中奖金额默认为空

--- pages/record/record.js
+++ pages/record/record.js
@@ -1,27 +1,105 @@
 Page({
-  data: { type: '', typeText: '', cost: '', winAmount: '', mahjongType: 'win', previewNet: 0 },
+  data: {
+    type: '',
+    typeText: '',
+    cost: '',
+    winAmount: '',
+    mahjongType: 'win',
+    previewNet: 0,
+    previewNetStr: '+0.00',
+    mode: 'add',
+    recordId: ''
+  },
+
   onLoad(options) {
     const type = options.type || 'lottery';
+    const mode = options.mode || 'add';
     const map = { lottery: '彩票', scratch: '刮刮乐', mahjong: '麻将' };
-    this.setData({ type, typeText: map[type], previewNetStr: '+0.00' });
+    
+    this.setData({
+      type,
+      typeText: map[type],
+      mode,
+      recordId: options.id || ''
+    });
+
+    // 编辑模式，加载原有数据
+    if (mode === 'edit' && options.id) {
+      this.loadRecord(options.id, type);
+    }
   },
-  onCostInput(e) { this.setData({ cost: e.detail.value }); this.calc(); },
-  onWinInput(e) { this.setData({ winAmount: e.detail.value }); this.calc(); },
-  setMahjongType(e) { this.setData({ mahjongType: e.currentTarget.dataset.type }); this.calc(); },
+
+  loadRecord(id, type) {
+    const db = wx.cloud.database();
+    db.collection(type).doc(id).get().then(res => {
+      const data = res.data;
+      if (data) {
+        const cost = Math.abs(data.cost || 0).toString();
+        // 中奖金额为0时显示空
+        const winAmount = data.winAmount === 0 ? '' : (data.winAmount || '').toString();
+        
+        this.setData({
+          cost,
+          winAmount,
+          mahjongType: data.amount >= 0 ? 'win' : 'lose'
+        });
+        this.calc();
+      }
+    });
+  },
+
+  onCostInput(e) {
+    this.setData({ cost: e.detail.value });
+    this.calc();
+  },
+
+  onWinInput(e) {
+    this.setData({ winAmount: e.detail.value });
+    this.calc();
+  },
+
+  setMahjongType(e) {
+    this.setData({ mahjongType: e.currentTarget.dataset.type });
+    this.calc();
+  },
+
   calc() {
     const { type, cost, winAmount, mahjongType } = this.data;
     let net = 0;
-    if (type === 'mahjong') net = (parseFloat(cost) || 0) * (mahjongType === 'win' ? 1 : -1);
-    else net = (parseFloat(winAmount) || 0) - (parseFloat(cost) || 0);
+    if (type === 'mahjong') {
+      net = (parseFloat(cost) || 0) * (mahjongType === 'win' ? 1 : -1);
+    } else {
+      // 中奖金额不填默认为0
+      net = (parseFloat(winAmount) || 0) - (parseFloat(cost) || 0);
+    }
     this.setData({
       previewNet: net,
       previewNetStr: (net >= 0 ? '+' : '') + net.toFixed(2)
     });
   },
+
   submit() {
-    const { type, cost, winAmount, mahjongType, previewNet } = this.data;
-    if (!cost) { wx.showToast({ title: '请输入金额', icon: 'none' }); return; }
-    wx.cloud.callFunction({ name: type, data: { action: 'add', cost, winAmount, mahjongType } })
-      .then(res => { if (res.result.success) { wx.showToast({ title: '保存成功' }); setTimeout(() => wx.navigateBack(), 1000); } });
+    const { type, cost, winAmount, mahjongType, mode, recordId } = this.data;
+    if (!cost) {
+      wx.showToast({ title: '请输入金额', icon: 'none' });
+      return;
+    }
+
+    const action = mode === 'edit' ? 'update' : 'add';
+    const data = { action, cost, winAmount: winAmount || '0', mahjongType };
+    if (mode === 'edit') data.id = recordId;
+
+    wx.cloud.callFunction({ name: type, data })
+      .then(res => {
+        if (res.result.success) {
+          wx.showToast({ title: mode === 'edit' ? '修改成功' : '保存成功' });
+          setTimeout(() => wx.navigateBack(), 1000);
+        } else {
+          wx.showToast({ title: '保存失败', icon: 'none' });
+        }
+      })
+      .catch(() => {
+        wx.showToast({ title: '保存失败', icon: 'none' });
+      });
   }
 });
