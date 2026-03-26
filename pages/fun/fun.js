cb4cb535a25b92f224094655d88ad87bb71c32d3
18million <584304337@qq.com>
2026-03-26 09:42:29 +0800

    refactor: 删除climbGame，游戏文件夹移到fun目录下

--- pages/fun/fun.js
+++ pages/fun/fun.js
@@ -2,15 +2,15 @@
 Page({
   data: {},
   goAlmostWin() {
-    wx.navigateTo({ url: '/pages/almostWin/almostWin' });
+    wx.navigateTo({ url: '/pages/fun/almostWin/almostWin' });
   },
   goIdiomGame() {
-    wx.navigateTo({ url: '/pages/idiomGame/idiomGame' });
+    wx.navigateTo({ url: '/pages/fun/idiomGame/idiomGame' });
   },
   goFeihuaGame() {
-    wx.navigateTo({ url: '/pages/feihuaGame/feihuaGame' });
+    wx.navigateTo({ url: '/pages/fun/feihuaGame/feihuaGame' });
   },
   goGame24() {
-    wx.navigateTo({ url: '/pages/game24/game24' });
+    wx.navigateTo({ url: '/pages/fun/game24/game24' });
   }
 });
