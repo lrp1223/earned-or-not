05a32818fe455a5904315b3f1f0cc106e936dc21
18million <584304337@qq.com>
2026-03-26 09:50:02 +0800

    fix: 游戏页面移回pages根目录，解决子目录注册问题

--- pages/fun/fun.js
+++ pages/fun/fun.js
@@ -2,15 +2,15 @@
 Page({
   data: {},
   goAlmostWin() {
-    wx.navigateTo({ url: '/pages/fun/almostWin/almostWin' });
+    wx.navigateTo({ url: '/pages/almostWin/almostWin' });
   },
   goIdiomGame() {
-    wx.navigateTo({ url: '/pages/fun/idiomGame/idiomGame' });
+    wx.navigateTo({ url: '/pages/idiomGame/idiomGame' });
   },
   goFeihuaGame() {
-    wx.navigateTo({ url: '/pages/fun/feihuaGame/feihuaGame' });
+    wx.navigateTo({ url: '/pages/feihuaGame/feihuaGame' });
   },
   goGame24() {
-    wx.navigateTo({ url: '/pages/fun/game24/game24' });
+    wx.navigateTo({ url: '/pages/game24/game24' });
   }
 });
